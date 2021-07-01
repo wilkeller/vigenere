bb912a7b671357960a1ab069b4f13391114a5a21
Wil Keller <keller.wse@gmail.com>
2021-06-30 19:31:15 -0600

    Initialize empty repo, stage notes/pseudocode

---
+++ vigenere.asm
@@ -0,0 +1,27 @@
+; Begin comment block
+;
+;
+;
+; End comment block
+
+; Build using these commands: 
+; nasm -f elf -g -F dwarf vigenere.asm
+; ld -o vigenere vigenere.o
+;
+
+section .data ; Section containing initialized data (i.e. variables)
+section .bss ; Section containing UNinitialized data
+section .text ; Section containing code 
+
+    global _start
+    
+_start:
+    nop
+; insert code between nops
+
+
+
+; insert code between nops
+    nop
+
+
