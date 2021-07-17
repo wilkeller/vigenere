98681b5bdeabb82c01ff80c7a69b36a79455372c
Wil Keller <56814785+wilkeller@users.noreply.github.com>
2021-07-16 19:03:53 -0600

    Fill initial comment; implement graceful exit

--- vigenere.asm
+++ vigenere.asm
@@ -1,8 +1,13 @@
-; Begin comment block
-;
-;
-;
-; End comment block
+; Author: Wil Keller
+; File: vigenere.asm
+; Description: an x86-64 program to encode/decode text from a user
+; specified input file using key data from a user specified keyfile, and
+; outputting the result into a user specified output file. This is my
+; 2nd ASM program, and, at the start of this, seems a little over my
+; head. The way I want to implement this will require argument parsing
+; and file I/O handling, as a minimum. This will take some doing. 
+; Date started: 16 JUL 21
+; Assembler: Netwide (NASM)
 
 ; Build using these commands: 
 ; nasm -f elf -g -F dwarf vigenere.asm
@@ -20,7 +25,10 @@ _start:
 ; insert code between nops
 
 
-
+; exit program gracefully
+    exit:   mov rax, 60    ; specify terminate sys_call
+            mov rdi, 0      ; pass 0 code ("success") to OS.
+            syscall         ; call sys_exit 
 ; insert code between nops
     nop
 
